f5db9ece1a0e4583a93c1b58676a4144ed830c27
delasimensah <mensadelasi@gmail.com>
2023-03-09 09:57:50 +0000

    added navbar component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,12 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { Layout } from "@components";
 
 const Home = () => {
-  return <><Link href='/product/samsung-galaxy-z-flip-3'>
-    Link To Product Page
-  </Link></>;
+  return (
+    <Layout>
+      <Link href="/product/samsung-galaxy-z-flip-3">Link To Product Page</Link>
+    </Layout>
+  );
 };
 
 export default Home;
